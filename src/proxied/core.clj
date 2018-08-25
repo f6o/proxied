3832a6a0b0f03d03f7b8265811b9ae5bc705f3c0
f6o <fluffy6h@gmail.com>
2018-08-25 07:41:42 +0000

    add filter

--- src/proxied/core.clj
+++ src/proxied/core.clj
@@ -1,9 +1,21 @@
 (ns proxied.core
  (:gen-class)
+ (:import (org.littleshoot.proxy HttpFiltersSourceAdapter HttpFiltersAdapter))
  (:import (org.littleshoot.proxy.impl DefaultHttpProxyServer))
  (:require [clojure.string :as str]))
 
 (defn -main
  ""
- [& args]
- (print (DefaultHttpProxyServer/bootstrap)))
+  [& args]
+  (println "START")
+  (.start
+   (.withFiltersSource
+    (.withPort
+     (DefaultHttpProxyServer/bootstrap)
+     18080)
+    (proxy [HttpFiltersSourceAdapter] []
+      (filterRequest [res ctx]
+        (proxy [HttpFiltersAdapter] [res]
+          (serverToProxyResponse [obj]
+            obj)))))))
+
